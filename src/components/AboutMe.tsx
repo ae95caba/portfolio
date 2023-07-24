fc4358a61647977dfe589fdd8fc55a86f544aff9
ae95caba <ae95caba@gmail.com>
2023-07-24 14:02:00 -0300

    add basic about me section layout

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,53 @@
 import React from "react";
 
 export default function AboutMe() {
-  return <section id="about">AboutMe</section>;
+  const goals = [
+    {
+      title: "Responsive",
+      description:
+        "fasdj asd asdf af fkljlkasdf asd  asdf  asdf   oaisdjfklajasdf ",
+    },
+    {
+      title: "Easy to mantain",
+      description: "fasdj asd fkljl af asdkasdf asdf oaisdjfklajasdf",
+    },
+    {
+      title: "Use cutting edge tech",
+      description: "fasdj asdfkl jl asdf kasdf  asdf oaisdjfklajasdf",
+    },
+    {
+      title: "Good looking",
+      description: "fasdj asdfkl jl asdf kasdf  adfs oaisdj fkla jasdf",
+    },
+  ];
+  return (
+    <section id="about">
+      <div className="background"></div>
+      <div className="content">
+        <h2>About me</h2>
+        <h3>My goal is to make apps that are</h3>
+        <div className="container">
+          {goals.map((goal, index) => (
+            <div className="subcontainer">
+              <Card goal={goal} index={index} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+interface Goal {
+  title: string;
+  description: string;
+}
+
+function Card({ goal, index }: { goal: Goal; index: number }) {
+  return (
+    <div className={`card card-${index % 2 === 0 ? "left" : "right"} `}>
+      <div className="tittle">{goal.title}</div>
+      <div className="description">{goal.description}</div>
+    </div>
+  );
 }
