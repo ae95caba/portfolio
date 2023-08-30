cb994deb114cd1a489dfb9407d3a2d5867e72b05
ae95caba <ae95caba@gmail.com>
2023-08-30 01:37:14 -0300

    add new project and make some minor style changes

--- src/assets/projects/projects.ts
+++ src/assets/projects/projects.ts
@@ -25,22 +25,31 @@ class Project {
 
 const project1 = new Project(
   "Dulce Tentacion",
-  "This is a online store for a real bussiness. It has user authentication and a shopping cart ",
+  "This is a online store for a real bussiness. It fetches the products from firebaseDb, has responsive design  and also a shopping cart ",
   new URL("./dulce-tentacion.png", import.meta.url).href,
   ["ReactJS", "SCSS", "Firebase"],
-  "https://github.com/ae95caba/Project-shopping-cart",
+  "https://github.com/ae95caba/dulce-tentacion",
   "https://dulce-tentacion-mp.netlify.app/"
 );
 
 const project2 = new Project(
-  "Dulce Tentacion",
-  "This is a online store for a real bussiness. It has user authentication and a shopping cart ",
-  new URL("./thumbnail2.jpg", import.meta.url).href,
-  ["ReactJS", "SCSS", "Firebase"],
-  "https://github.com/ae95caba/Project-shopping-cart",
-  "https://dulce-tentacion-mp.netlify.app/"
+  "Portfolio",
+  "This is the code for this page. Its fully responsive on most devices. Technologies used are listed below. ",
+  new URL("../../../public/portfolio.png", import.meta.url).href,
+  ["ReactJS", "SCSS", "TypeScript"],
+  "https://github.com/ae95caba/portfolio",
+  "https://andreespinozadev.netlify.app/"
 );
 
-const projectsArr = [project1, project1];
+const project3 = new Project(
+  "Milei Marcos Paz",
+  "This is a real page I've made for a politician, I showcases a lot of text about this proposals and projects. It has a minimalisitc but yet elegant design ",
+  new URL("./milei-mp.png", import.meta.url).href,
+  ["ReactJS", "SCSS", "TypeScript"],
+  "https://github.com/ae95caba/milei-mp",
+  "https://milei-jose.netlify.app/"
+);
+
+const projectsArr = [project1, project2, project3];
 
 export default projectsArr;
